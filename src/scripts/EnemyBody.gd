a0c2fdbe8e349e556e75c9dcfa6764969ffc0ef1
Tai <dormoxx@gmail.com>
2022-01-10 21:51:22 -0500

    before branching into v2

--- src/scripts/EnemyBody.gd
+++ src/scripts/EnemyBody.gd
@@ -2,19 +2,21 @@ extends "res://src/scripts/Actor.gd"
 
 #todo : VisibilityEnabler2D: https://www.youtube.com/watch?v=Mc13Z2gboEk&t=4768s
 
-onready var stompArea = $StompArea
-onready var stompColl = $StompArea/StompCollider
+onready var stompDetector: Area2D = $StompDetector
+
 export var scoreValue:= 100
 func _ready() -> void:
-	_speed.x = 350
+	_speed.x = 150
 	_velocity.x = -_speed.x
 
-func _on_StompArea_body_entered(body: Node) -> void:
-	if body.global_position.y > stompArea.global_position.y:
+func _on_StompDetectorArea_body_entered(body: PhysicsBody2D) -> void:
+	if body.global_position.y > stompDetector.global_position.y:
 		return
 	die()
 
+
 func _physics_process(delta: float) -> void:
+	#_velocity = Vector2.ZERO
 	if !is_on_floor(): #apply gravity only when in the air
 		_velocity.y += _gravity * delta
 	if is_on_wall():
@@ -25,3 +27,4 @@ func die():
 	queue_free()
 	PlayerData.score += scoreValue
 	PlayerData.increase_combo()
+
--- src/scripts/PlayerBody.gd
+++ src/scripts/PlayerBody.gd
@@ -4,24 +4,27 @@ export var stomp_impulse:= 1250
 var stomp_increment:= 250
 var combo_pitch
 var scaled_speed: Vector2 = _speed
-var speed_scale = 100
+export(Vector2) var speed_scale = Vector2(25, 25)
+onready var enemyDetector: Area2D = $EnemyDetector
 
-var is_midair = false
+var is_midair: bool
 
 func _ready() -> void:
 	PlayerData.connect("combo_increased", self, "peggle_bongs")
 	PlayerData.combo_count = 0
+	#Engine.time_scale = 0.1
 
-func _on_EnemyDetectorArea_area_entered(_area: Area2D) -> void:
+func _on_EnemyDetector_area_entered(area: Area2D) -> void:
 	_velocity = calc_stomp_velocity(_velocity, stomp_impulse)
+	if _velocity.y != 0:
+		is_midair = true
 
-func _on_EnemyDetectorArea_body_entered(_body: Node) -> void:
+func _on_EnemyDetector_body_entered(body: Node) -> void:
 	die()
-
 
 func _physics_process(delta: float) -> void:
 	if Input.is_action_just_released("reset"):
-		get_tree().reload_current_scene()
+		get_tree().reload_current_scene() #hacky
 	
 	var jump_interrupt:= Input.is_action_just_released("jump") and _velocity.y < 0.0
 	var direction:= get_direction()
@@ -42,33 +45,35 @@ func calc_movement(velocity: Vector2, direction: Vector2, speed: Vector2, jump_c
 		new_velocity.x = scaled_speed.x * direction.x
 	
 	#override jump behavior (-1.0 from get_input())
-	if direction.y == -1.0 and !is_midair:
+	if direction.y == -1.0:
 		new_velocity.y = speed.y * direction.y
 		is_midair = true
 	
 	if !is_on_floor(): #only apply gravity while not on floor
 		new_velocity.y += _gravity * delta
+
 	
 	if jump_canceled: #player stops holding jump
 		new_velocity.y = 0.0
 	
-	if new_velocity.y > 0.0: #fast fall
+	if new_velocity.y >= 0.0: #fast fall
 		if Input.is_action_just_pressed("down"):
 			new_velocity.y += speed.y * direction.y
-	if new_velocity.y == 0 and is_on_floor():
+	if is_on_floor() and new_velocity.y == 0:
 		is_midair = false
 	return new_velocity
 
 func calc_stomp_velocity(velocity: Vector2, impulse: float) -> Vector2:
 	var new_vel:= velocity
-	#if new_vel.y > 0.0: #only apply stomp if player is falling
-	
+
 	if PlayerData.combo_count == 0 or 1:
 		new_vel.y = -(impulse)
 	if PlayerData.combo_count >= 2:
 		new_vel.y = -(impulse + (PlayerData.combo_count * stomp_increment))
-		scaled_speed.x += (speed_scale * PlayerData.combo_count)
-		scaled_speed.y += (speed_scale * PlayerData.combo_count)
+		scaled_speed.x += (speed_scale.x * PlayerData.combo_count)
+		scaled_speed.y += (speed_scale.y * PlayerData.combo_count)
+	if new_vel.y != 0:
+		is_midair = true
 	return new_vel
 
 func verify_combo():
@@ -87,4 +92,12 @@ func peggle_bongs():
 func die():
 	PlayerData.deaths += 1
 	queue_free()
+	#get_tree().reload_current_scene()
 	
+	
+
+
+
+
+
+
